15adbf887eaa9459bbc8464d696c97fc8da3c1ea
Vasundhra04 <113412992+Vasundhra04@users.noreply.github.com>
2025-02-05 17:11:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+//import logo from './logo.svg';
+import './App.css';
+import Login from  './ReactComponents/Login';
+import Enjoy from './ReactComponents/Enjoy';
+import Accomadation from "./ReactComponents/Accomadation";
+import Choice from "./ReactComponents/Choice";
+import Profile from "./ReactComponents/Profile";
+import Signup from "./ReactComponents/Signup";
+import Reset from "./ReactComponents/Reset";
+import Entertainment from "./ReactComponents/Entertainment";
+import Food from "./ReactComponents/Food";
+import Transport from "./ReactComponents/Transport";
+import  Basicneeds from "./ReactComponents/Basicneeds";
+import Likedtrips from "./ReactComponents/Likedtrips";
+import Router from "./ReactComponents/Router";
+function App() {
+  return (
+    <div>
+      <Router/>
+     
+    </div>
+  );
+}
+
+export default App;
